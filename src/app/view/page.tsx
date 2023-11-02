73e37071e3e4d92e3a99da827ca5effe2b12ff53
Natalia Francisca <nathifrancisca@gmail.com>
2023-11-02 17:36:05 -0300

    feat: create the page view; init the notebook design

--- src/app/view/page.tsx
+++ src/app/view/page.tsx
@@ -1,10 +1,61 @@
 'use client';
+import Circle from "../components/circle/circle";
 import { ViewComponent } from "./view.css";
 
 const View = () => {
+
     return(
         <ViewComponent>
-            <h1>TESTES</h1>
+            <section id="lines">
+                <div className="line" />
+                <div className="line" />
+                <div className="line" />
+                <div className="line" />
+                <div className="line" />
+                <div className="line" />
+                <div className="line" />
+                <div className="line" />
+                <div className="line" />
+                <div className="line" />
+                <div className="line" />
+                <div className="line" />
+                <div className="line" />
+                <div className="line" />
+            </section>
+
+            <section className="column" id="column-1">
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+            </section>
+
+            <section className="column" id="column-2">
+                <br />
+                <p>NATALIAS TOP FIVE</p>
+                <br />
+                <h1>DA LUA</h1>
+                <br />
+                <h1>MERCADO</h1>
+                <br />
+                <h1>ANOTHER LOVE</h1>
+                <br />
+                <h1>MONACO</h1>
+                <br />
+                <h1>CHOROJO</h1>
+                <br />
+            </section>
+
         </ViewComponent>
     )
 }
