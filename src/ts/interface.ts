f139b3aecf42cb9996db2f0968b384792ad7f869
Natalia Francisca <nathifrancisca@gmail.com>
2023-11-04 20:53:46 -0300

    feat: create the interface file

---
+++ src/ts/interface.ts
@@ -0,0 +1,23 @@
+export interface INumberColor{
+    fill: string,
+    stroke: string
+}
+
+export interface ISong{
+    index: number;
+    song_name: string;
+    song_artist: string;
+    img: string;
+}
+
+export interface IViewTheme{
+    punched_color: string,
+    paper_color: string,
+    paper_line_color?: string,
+    text_color: string,
+
+    number_color: {
+        fill: string,
+        stroke: string
+    }
+}
