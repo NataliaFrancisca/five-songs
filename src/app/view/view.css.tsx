45eb4df6bb6209cf9686f2e21cab9c43553eae5a
Natalia Francisca <nathifrancisca@gmail.com>
2023-11-04 20:52:43 -0300

    feat: use the props at the view component

--- src/app/view/view.css.tsx
+++ src/app/view/view.css.tsx
@@ -1,13 +1,15 @@
+import { IViewTheme } from "@/ts/interface";
 import styled from "styled-components";
 
-export const ViewComponent = styled.main`
-    background-color: var(--white-color);
+export const ViewComponent = styled.main<{ $theme?: IViewTheme; }>`
+    background-color: ${props => props.$theme?.paper_color};
     min-height: 100vh;
     height: auto;
     display: grid;
     position: relative;
+    color: ${props => props.$theme?.text_color};
 
-    grid-template-columns: 83px auto;
+    grid-template-columns: 78px auto;
 
     #lines{
         display: grid;
@@ -15,50 +17,28 @@ export const ViewComponent = styled.main`
         grid-template-rows: repeat(14, 59px);
         width: 100%;
 
-        .line{
+        div{
             height: 100%;
-            border-bottom: solid 2px rgba(26, 27, 28, 0.70);
+            border-bottom: solid 2px ${props => props.$theme?.paper_line_color ? props.$theme.paper_line_color : 'rgba(26, 27, 28, 0.70)'}
         }
 
-        .line:last-child{
+        div:last-child{
             border-bottom: none;
         }
     }
 
-    .column{
+    #punched-paper-column{
         display: grid;
         grid-template-rows: repeat(14, 59px);
-    }
-
-
-    #column-1{
         border-right: 2px solid rgba(194, 45, 45, 0.57);
+
+
+        & div{
+            width: 24px;
+            height: 24px;
+            margin: 18px 30px 18px 30px;
+            border-radius: 50%;
+            background-color: ${props => props.$theme?.punched_color};
+        }
     }
-
-    #column-2{
-        margin-left: 10px;
-        align-items: center;
-    }
-
-
-/* 
-    display: grid;
-    grid-template-columns: 83px auto;
-
-    .row-1{
-        border-bottom: solid 2px rgba(26, 27, 28, 0.70);
-    }
-
-    #column-2{
-        display: grid;
-        grid-template-rows: repeat(14, 59px);
-    } 
-
-    #column-1{
-        display: grid;
-        align-items: center;
-        justify-content: center;
-        grid-template-rows: repeat(14, 59px);
-    } */
-
 `
