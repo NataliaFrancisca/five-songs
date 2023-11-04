06bc993f05e704560c93c3d0ace86c900d479d42
Natalia Francisca <nathifrancisca@gmail.com>
2023-11-04 20:52:25 -0300

    feat: create the view page

--- src/app/view/page.tsx
+++ src/app/view/page.tsx
@@ -1,60 +1,92 @@
 'use client';
-import Circle from "../components/circle/circle";
+import Songs from "../components/songs/songs";
 import { ViewComponent } from "./view.css";
 
+const paper_theme = {
+    white: {
+        punched_color: '#16120F',
+        paper_color: '#FDF6EC',
+        text_color: '#1A1B1C',
+
+        number_color: {
+            fill: '#9974F8',
+            stroke:'#16120F'
+        }
+    },
+
+    black: {
+        punched_color: '#FDF6EC',
+        paper_color: '#16120F',
+        paper_line_color: 'rgba(253, 246, 236, 0.60)',
+        text_color: '#FDF6EC',
+
+        number_color: {
+            fill: '#C6F277',
+            stroke: '#000000'
+        }
+    },
+
+    green: {
+        punched_color: '#16120F',
+        paper_color: '#C6F277',
+        text_color: '#1A1B1C',
+
+        number_color: {
+            fill: '#16120F',
+            stroke: '#FDF6EC'
+        }
+    },
+
+    purple: {
+        punched_color: '#16120F',
+        paper_color: '#9A73F9',
+        text_color: '#1A1B1C',
+
+        number_color: {
+            fill: '#FDF6EC',
+            stroke: '#16120F'
+        }
+    }
+}
+
 const View = () => {
-
     return(
-        <ViewComponent>
+        <ViewComponent $theme={paper_theme.white}>
             <section id="lines">
-                <div className="line" />
-                <div className="line" />
-                <div className="line" />
-                <div className="line" />
-                <div className="line" />
-                <div className="line" />
-                <div className="line" />
-                <div className="line" />
-                <div className="line" />
-                <div className="line" />
-                <div className="line" />
-                <div className="line" />
-                <div className="line" />
-                <div className="line" />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
             </section>
 
-            <section className="column" id="column-1">
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
+            <section id="punched-paper-column">
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
             </section>
 
-            <section className="column" id="column-2">
-                <br />
-                <p>NATALIAS TOP FIVE</p>
-                <br />
-                <h1>DA LUA</h1>
-                <br />
-                <h1>MERCADO</h1>
-                <br />
-                <h1>ANOTHER LOVE</h1>
-                <br />
-                <h1>MONACO</h1>
-                <br />
-                <h1>CHOROJO</h1>
-                <br />
-            </section>
+            <Songs numberColor={paper_theme.white.number_color} />
 
         </ViewComponent>
     )
