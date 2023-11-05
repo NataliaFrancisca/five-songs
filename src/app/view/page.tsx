56390e9b02ce74dd5ba1cc1caf878763ceffb229
Natalia Francisca <nathifrancisca@gmail.com>
2023-11-05 20:25:25 -0300

    feat: remove the style variables; add the functions to update the view template

--- src/app/view/page.tsx
+++ src/app/view/page.tsx
@@ -1,92 +1,59 @@
 'use client';
+import { getThemeCookie } from "@/storage/theme";
 import Songs from "../components/songs/songs";
 import { ViewComponent } from "./view.css";
-
-const paper_theme = {
-    white: {
-        punched_color: '#16120F',
-        paper_color: '#FDF6EC',
-        text_color: '#1A1B1C',
-
-        number_color: {
-            fill: '#9974F8',
-            stroke:'#16120F'
-        }
-    },
-
-    black: {
-        punched_color: '#FDF6EC',
-        paper_color: '#16120F',
-        paper_line_color: 'rgba(253, 246, 236, 0.60)',
-        text_color: '#FDF6EC',
-
-        number_color: {
-            fill: '#C6F277',
-            stroke: '#000000'
-        }
-    },
-
-    green: {
-        punched_color: '#16120F',
-        paper_color: '#C6F277',
-        text_color: '#1A1B1C',
-
-        number_color: {
-            fill: '#16120F',
-            stroke: '#FDF6EC'
-        }
-    },
-
-    purple: {
-        punched_color: '#16120F',
-        paper_color: '#9A73F9',
-        text_color: '#1A1B1C',
-
-        number_color: {
-            fill: '#FDF6EC',
-            stroke: '#16120F'
-        }
-    }
-}
+import { useEffect, useState } from "react";
+import { IThemeJSON } from "@/ts/interface";
 
 const View = () => {
-    return(
-        <ViewComponent $theme={paper_theme.white}>
-            <section id="lines">
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-            </section>
+    const [currentViewTheme, setCurrentViewTheme] = useState<IThemeJSON>();
 
-            <section id="punched-paper-column">
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-            </section>
+    const onUpdateView = () => {
+        const viewThemeResult = getThemeCookie();
+        setCurrentViewTheme(viewThemeResult);
+    }
 
-            <Songs viewTheme={paper_theme.white} />
+    useEffect(() => {
+        onUpdateView();
+    },[])
 
+    return( currentViewTheme &&
+        <ViewComponent $theme={currentViewTheme.theme}>
+            <div id="lines">
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+            </div>
+
+            <div id="punched-paper-column">
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+            </div>
+  
+            {currentViewTheme && <Songs viewTheme={currentViewTheme.theme} updateView={onUpdateView} />}
         </ViewComponent>
     )
 }
