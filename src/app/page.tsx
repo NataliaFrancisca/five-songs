be393b424000bdeb21bc5f03df49689679395d4b
Natalia Francisca <nathifrancisca@gmail.com>
2023-11-06 22:51:58 -0300

    feat: remove the default theme; get the default theme from a css file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,14 @@
 import { GlobalStyle } from "@/styles/global.css";
 import Login from "./components/login/login";
 import { getThemeCookie, setThemeCookie } from "@/storage/theme";
+import { PAPER_THEME } from "@/styles/theme.css";
 
 export default function Home() {
-
-  const defaultTheme = {
-      punched_color: '#16120F',
-      paper_color: '#FDF6EC',
-      text_color: '#1A1B1C',
-      number_color: {
-        fill: '#9974F8',
-        stroke:'#16120F'
-      }
-  }
 
   const userThemer = getThemeCookie();
 
   if(!userThemer){
-    setThemeCookie(defaultTheme, 'WHITE');
+    setThemeCookie(PAPER_THEME[0]);
   }
 
   return (
