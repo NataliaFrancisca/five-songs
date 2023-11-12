7999a8abe79ed3a7ed08c6b9bd082908e7e32e9d
Natalia Francisca <nathifrancisca@gmail.com>
2023-11-12 20:48:35 -0300

    feat(style): crete a style for the album cover with the sticker;

--- src/app/components/songs/songs.css.tsx
+++ src/app/components/songs/songs.css.tsx
@@ -1,9 +1,8 @@
-import { handleStickerColor } from "@/styles/theme.css";
 import styled from "styled-components";
 
 export const SongsComponent = styled.section`
     display: grid;
-    grid-template-rows: repeat(10, 68px);
+    grid-template-rows: repeat(9, 68px);
     align-items: center;
     z-index: 11;
     width: 100%;
@@ -14,11 +13,11 @@ export const SongsComponent = styled.section`
         align-items: center;
     }
 
-    h1{
-        font-family: var(--font-londrina);
-        text-transform: uppercase;
-        font-weight: 300;
-        font-size: 34px;
+    h1#user-title{
+        font-size: 28px;
+        font-family: var(--font-spline-sans-mono);
+        font-weight: bold;
+        color: #1A1B1C;
         text-align: center;
     }
 
@@ -41,68 +40,19 @@ export const ImageElement = styled.div`
     align-items: center;
     position: relative;
     justify-self: center;
-    width: 158px;
-    height: 158px;
 
-    img{
-        height: 100%;
-        width: 100%;
+    #album-cover-img{
+        width: 174px;
+        height: 172px;
     }
 
-
-    svg:first-child{
+    .sticker-img:first-child{
         position: absolute;
         top: -10px;
     }
 
-    svg:last-child{
+    .sticker-img:last-child{
         position: absolute;
         bottom: -10px;
     }
 `
-
-// export const ImageElement = styled.div<{$url: string, $stickerColor: string}>`
-//     width: 165px;
-//     height: 148px;
-//     object-fit: cover;
-//     box-shadow: 2px 1px 1px 0px rgba(0, 0, 0, 0.25);
-//     display: flex;
-//     background-image: url(${props => props.$url});
-//     position: relative;
-//     margin: 0 auto;
-
-//     &::before, &::after{
-//         content: "";
-//         background-image: ${props => handleStickerColor(props.$stickerColor)};
-//         background-repeat: no-repeat;
-//         background-size: contain;
-//         display: block;
- 
-//         width: 63px;
-//         flex-shrink: 0;
-//         position: relative;
-//         z-index: 1111;
-//     }
-
-//     &::before{
-//         top: -18px;
-//         left: -18px;
-//         transform: rotate(-5.613deg);
-//     }
-
-//     &::after{
-//         top: 128px;
-//         left: 70px;
-//         transform: rotate(-1.613deg);
-//     }
-
-//     @media screen and (max-width: 376px){
-//         width: 155px;
-
-//         &::after{
-//             top: 128px;
-//             left: 40px;
-//             transform: rotate(-1.613deg);
-//         }
-//     } 
-// `
