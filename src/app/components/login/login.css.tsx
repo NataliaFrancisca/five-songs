9a0db4d0da1a099dd4d52b743ae2f7091fb84a36
Natalia Francisca <nathifrancisca@gmail.com>
2023-11-12 20:20:23 -0300

    feat(style): create style for the text; remove styles for images

--- src/app/components/login/login.css.tsx
+++ src/app/components/login/login.css.tsx
@@ -7,44 +7,47 @@ export const LoginComponent = styled.main`
     display: flex;
     align-items: flex-end;
 
-    .box-login{
-        width: 30%;
-        margin-bottom: 70px;
-        align-items: flex-end;
+    h1{
+        font-size: 64px;
+        font-family: var(--font-spline-sans-mono);
+        font-weight: 700;
+        color: #FCFE06;
     }
 
-    .message-login{
-        align-items: center;
-        color: var(--white-color);
-        display: flex;
+    h2{
         font-size: 24px;
-        gap: 17px;
+        font-weight: 400;
+        color: var(--white-color);
 
-        & img{
-            height: 56px;
-            width: 34px;
-        }
+        display: flex;
+        align-items: center;
+        gap: 6px;
+
+        letter-spacing: 2px;
     }
 
-    img{
-        width: 180px;
+    span#subtitle_styled{
+        font-size: 54px;
+        font-family: var(--font-spline-sans-mono);
+        font-weight: 700;
+        color: #FCFE06;
     }
 
     button{
         background-color: #FFFFFF;
         border: none;
-        border-radius: 20px;
+        border-radius: 4px;
         color: var(--black-color);
         cursor: pointer;
-        font-size: 20px;
-        font-weight: bold;
-        margin-top: 30px;
-        padding: 24px 0;
+        font-size: 16px;
+        font-weight: 700;
+        margin: 20px 0;
+        padding: 20px 0;
         transition: all ease-in-out .2s;
         width: 100%;
 
         &:hover{
-            background-color: var(--primary-color);
+            background-color: #FCFE06;
             transition: all ease-in-out .6s;
         }
     }
