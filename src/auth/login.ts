6b306afda6b76d0515dc0ac8886c8177a2b4a0c2
Natalia Francisca <nathifrancisca@gmail.com>
2023-11-13 17:15:18 -0300

    feat: functions to connect with the spotify

---
+++ src/auth/login.ts
@@ -0,0 +1,31 @@
+import { generateRandomString, sha256, base64encode } from "./crypto/crypto";
+
+async function loginSpotify(){
+    const client_id = process.env.NEXT_PUBLIC_CLIENT_ID;
+    const redirect_uri = "http://localhost:3000/view";
+
+    const scope = 'user-read-private user-read-email user-top-read';
+    const authUrl = new URL("https://accounts.spotify.com/authorize")
+
+    const codeVerifier  = generateRandomString(64);
+    window.localStorage.setItem('code_verifier', codeVerifier);
+
+    const hashed = await sha256(codeVerifier)
+    const codeChallenge = base64encode(hashed);
+
+    if(client_id){
+        const params =  {
+            response_type: 'code',
+            client_id: client_id,
+            scope,
+            code_challenge_method: 'S256',
+            code_challenge: codeChallenge,
+            redirect_uri: redirect_uri,
+        }
+
+        authUrl.search = new URLSearchParams(params).toString();
+        window.location.href = authUrl.toString();
+    }
+}
+
+export default loginSpotify;
