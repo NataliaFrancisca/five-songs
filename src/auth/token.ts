25d256d31c814c8d06021beed3e88964543fd06e
Natalia Francisca <nathifrancisca@gmail.com>
2023-11-18 20:03:56 -0300

    feat: create a function to get the current token or save the token

--- src/auth/token.ts
+++ src/auth/token.ts
@@ -30,3 +30,18 @@ export async function getUserToken(){
         }
     }
 }
+
+export async function getToken(){
+  const current_token = localStorage.getItem("USER_TOKEN");
+
+  if(current_token){
+      return await JSON.parse(current_token);
+  }else{
+      try{
+          await getUserToken();
+          return await getToken();
+      }catch(error: any){
+          console.log(error?.message);
+      }
+  }
+}
