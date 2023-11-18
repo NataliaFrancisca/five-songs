d2d9c8626ac3a61a5e33e84b14c8b645b30d4e58
Natalia Francisca <nathifrancisca@gmail.com>
2023-11-17 21:46:05 -0300

    fix: show all the artists

--- src/app/components/song/song.tsx
+++ src/app/components/song/song.tsx
@@ -10,7 +10,10 @@ const Song = (obj: {song: ISong}) => {
 
             <div className="group-artist-info">
                 <h2 className="bold">{song_name}</h2>
-                <h2>{song_artist}</h2>
+                <h2 className="artist-name">{song_artist.map((artist, key) => (
+                    <span key={key}>{artist.name}</span>
+                ))}
+                </h2>
             </div>
         </SongComponent>
     )
