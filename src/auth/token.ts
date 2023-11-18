465a6ea167bc99450885593d6c89ad07bbeb1bd0
Natalia Francisca <nathifrancisca@gmail.com>
2023-11-18 20:11:41 -0300

    feat: return error message; function must return a string

--- src/auth/token.ts
+++ src/auth/token.ts
@@ -31,17 +31,17 @@ export async function getUserToken(){
     }
 }
 
-export async function getToken(){
+export async function getToken():Promise<string | undefined>{ 
   const current_token = localStorage.getItem("USER_TOKEN");
 
   if(current_token){
       return await JSON.parse(current_token);
   }else{
       try{
-          await getUserToken();
-          return await getToken();
+        await getUserToken();
+        await getToken();
       }catch(error: any){
-          console.log(error?.message);
+        return error?.message;
       }
   }
 }
