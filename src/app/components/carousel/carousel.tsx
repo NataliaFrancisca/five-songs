6644da5d9da2a40ed0011f4a52ebbc4c40529b50
Natalia Francisca <nathifrancisca@gmail.com>
2023-11-20 22:16:21 -0300

    feat: remove the notebook component and use the elements inside a styled component

--- src/app/components/carousel/carousel.tsx
+++ src/app/components/carousel/carousel.tsx
@@ -1,20 +1,31 @@
 import Image from "next/image";
 import { UseCarousel } from "@/hooks/useCarousel";
 import { PAPER_THEME } from "@/styles/theme.css";
-import NotebookSheet from "../notebook-sheet/notebook-sheet";
+import PaperBackground from "@/app/ui/paper-background";
+import Songs from "../songs/songs";
+import { INotebookInfo } from "@/ts/interface";
+import { UseRefNotebook } from "@/hooks/useRef";
+import { NotebookSheetBG } from "@/app/view/view.css";
+import { CarouselComponent } from "./carousel.css";
 
-const Carousel = (props: {currentElement: number}) => {
+const Carousel = (props: {currentElement: number, carouselData: INotebookInfo}) => {
 
     const { currentCarouselElement, onPrev, onNext } = UseCarousel(props.currentElement);
+    const { refNotebook } = UseRefNotebook();
 
     return(
-        <>
+        <CarouselComponent>
             <section id="carousel">
                 <button onClick={() => onPrev()}>
                     <Image src="icon/arrow-left.svg" alt="icon download" width={10} height={18}/>
                 </button>
                 
-                <NotebookSheet paperStyle={PAPER_THEME[currentCarouselElement]} />
+                <NotebookSheetBG $paperTheme={PAPER_THEME[currentCarouselElement]} ref={refNotebook}>
+                    <PaperBackground />
+                    <Songs userName={props.carouselData.userName} songsList={
+                        props.carouselData.listSongs
+                    }/>
+                </NotebookSheetBG>
 
                 <button onClick={() => onNext()}>
                     <Image src="icon/arrow-right.svg" alt="icon download" width={10} height={18}/>
@@ -26,7 +37,7 @@ const Carousel = (props: {currentElement: number}) => {
                     element.id == currentCarouselElement ? <span key={element.id} className="current-page"/> : <span key={element.id} />
                 ))}
             </section>
-        </>
+        </CarouselComponent>
     )
 }
 
