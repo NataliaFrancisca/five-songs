5f9063a9e25e3acb5e5e31ffa19f57b49a664151
Natalia Francisca <nathifrancisca@gmail.com>
2023-11-22 22:06:28 -0300

    feat: change the way to get the current theme

--- src/app/view/page.tsx
+++ src/app/view/page.tsx
@@ -3,39 +3,33 @@ import { UseProfile } from "@/hooks/useProfile";
 import Image from "next/image";
 import { htmlToImageConvert } from "@/utils/convert-image";
 import { ViewComponent } from "./view.css"
-import { useEffect, useState } from "react";
-import { IPaperStyle } from "@/ts/interface";
 import Loader from "../ui/loader";
 import { useRefContext } from "@/context/ref-context";
 import { getTheme } from "@/utils/get-theme";
 import Carousel from "../components/carousel/carousel";
 
-
 const View = () => {
     const { loading, notebookInfo} = UseProfile();
-    const [currentElementStyle, setCurrentElementStyle] = useState<IPaperStyle>(); 
     const { refContext } = useRefContext();
+
+    const theme = getTheme();
+
 
     const convertHtmlToImage = async() => {
         await htmlToImageConvert(refContext);
     }
-    
-    useEffect(() => {
-        const response = getTheme();
-        setCurrentElementStyle(response);
-    },[])
 
     return(
         <ViewComponent>
-            {loading && <Loader $colorTheme={currentElementStyle?.color}/>}
+            {loading && <Loader $colorTheme={theme?.color}/>}
 
-            {!loading && currentElementStyle && notebookInfo &&
+            {!loading && theme && notebookInfo &&
                 <>
                     <button id="btn-download" onClick={() => convertHtmlToImage()}>
                         <Image src="icon/download.svg" alt="icon download" width={20} height={20}/>
                     </button>
 
-                    <Carousel currentElement={currentElementStyle.id} carouselData={notebookInfo}/>
+                    <Carousel currentElement={theme.id} carouselData={notebookInfo}/>
                 </>
             }
 
