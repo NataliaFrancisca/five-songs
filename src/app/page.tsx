a758bfdcb750f2bdac551a908b7cfbb4ddc3e32c
Natalia Francisca <nathifrancisca@gmail.com>
2023-11-24 20:46:35 -0300

    feat: remove the setTheme function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
 'use client';
-import { GlobalStyle } from "@/styles/global.css";
 import Login from "./components/login/login";
-import { getThemeCookie, setThemeCookie } from "@/storage/theme";
-import { PAPER_THEME } from "@/styles/theme.css";
 
 export default function Home() {
 
-  const userThemer = getThemeCookie();
-
-  if(!userThemer){
-    setThemeCookie(PAPER_THEME[0]);
-  }
-
   return (
     <>
-        <Login />
+      <Login />
     </>
   )
 }
