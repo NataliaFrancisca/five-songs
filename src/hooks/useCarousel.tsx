58f8693101138cd2bad772680e73d42bfd43d760
Natalia Francisca <nathifrancisca@gmail.com>
2023-11-24 20:46:08 -0300

    feat: update the functions related with style theme

--- src/hooks/useCarousel.tsx
+++ src/hooks/useCarousel.tsx
@@ -1,4 +1,4 @@
-import { setThemeCookie } from "@/storage/theme";
+import { setTheme } from "@/storage/theme";
 import { PAPER_THEME } from "@/styles/theme.css";
 import { useState } from "react";
 
@@ -8,23 +8,23 @@ export const UseCarousel = (currentElement: number) => {
     const onPrev = () => {
         if(currentCarouselElement == 0){
             setCurrentCarouselElement(PAPER_THEME.length - 1);
-            setThemeCookie(PAPER_THEME[PAPER_THEME.length - 1]);
+            setTheme(PAPER_THEME[PAPER_THEME.length - 1]);
             return;
         }
 
         setCurrentCarouselElement((prev:number) => prev - 1);
-        setThemeCookie(PAPER_THEME[currentCarouselElement - 1]);
+        setTheme(PAPER_THEME[currentCarouselElement - 1]);
     }
 
     const onNext = () => {
         if(currentCarouselElement == PAPER_THEME.length - 1){
             setCurrentCarouselElement(0);
-            setThemeCookie(PAPER_THEME[0]);
+            setTheme(PAPER_THEME[0]);
             return;
         }
 
         setCurrentCarouselElement((prev:number) => prev + 1);
-        setThemeCookie(PAPER_THEME[currentCarouselElement + 1]);
+        setTheme(PAPER_THEME[currentCarouselElement + 1]);
     }
 
     return { currentCarouselElement, onPrev, onNext }
