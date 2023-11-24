07870d83a0da7d1ed11d179dd69ad89b9d1b24d6
Natalia Francisca <nathifrancisca@gmail.com>
2023-11-23 21:07:39 -0300

    feat: create function to validate the current token;

--- src/hooks/useProfile.tsx
+++ src/hooks/useProfile.tsx
@@ -10,63 +10,47 @@ export const UseProfile = () => {
 
     const router = useRouter();
 
+    async function fetchData(){
+        const result = await validateCurrentToken();
 
-    async function userProfile(token: string){
-        const response = await getUserProfile(token);
-        
-        if(response == 'The access token expired'){
-            router.push("/");
-            return;
-        }   
-
-        if(response == 'Invalid access token' && token){
-            router.refresh();
-            return;
-        }
-        
-        return response.display_name;
-    }
-
-    async function userTopFive(token: string){
-        const response = await getUserTopFive(token);
-        
-        if(response == 'The access token expired'){
-            router.push("/");
-            return;
-        }
-
-        if(response == 'Invalid access token' && token){
-            router.refresh();
-            return;
-        }
-
-        return response;
-    }
-
-
-    async function fetch_data(){
-        const token_storage = localStorage.getItem("USER_TOKEN");
-
-        if(token_storage){
-            const [userName, listSongs] = await Promise.all([userProfile(token_storage), userTopFive(token_storage)]);
-            setNotebookInfo({userName, listSongs});
+        if(result){
+            const [userName, listSongs] = await Promise.all([getUserProfile(result), getUserTopFive(result)]);
+            setNotebookInfo({userName: userName.display_name, listSongs});
             setLoading(false);
         }
+    }
 
+    async function validateCurrentToken(){
+        const token_storage = localStorage.getItem("USER_TOKEN");
+
+        if(!token_storage && window.location.search == ""){
+            console.log("NOT HERE");
+            router.push("/");
+        }
 
         if(!token_storage){
             await setUserToken();
-            // window.location.reload();
-        }
+            window.location.reload();
+        } 
+        
+        if(token_storage){
+            const response = await getUserProfile(token_storage);
 
-        if(token_storage == null){
-            router.push("/");
-        }
+            switch(response){
+                case 'The access token expired':
+                    router.push("/");
+                    break;
+                case 'Invalid access token':
+                    router.refresh();
+                    break;
+            }
 
+            return token_storage;
+        }    
     }
 
     useEffect(() => {
-        fetch_data();
+        fetchData();
     },[])
 
 
