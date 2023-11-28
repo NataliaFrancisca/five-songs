d7b89a4cb9e9cea624ab1cfae9253ccccaa6cf78
Natalia Francisca <nathifrancisca@gmail.com>
2023-11-27 23:36:54 -0300

    feat(style): create a style for error message

--- src/app/view/view.css.tsx
+++ src/app/view/view.css.tsx
@@ -71,3 +71,43 @@ export const NotebookSheetBG = styled.section<{$paperTheme?: IPaperStyle}>`
         }
     }
 `
+
+export const ErrorMessageStyle = styled.div`
+    background-color: var(--white-color);
+    padding: 20px;
+    border-radius: 4px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+
+    h1:before{
+        content: "";
+        height: auto;
+        width: 10px;
+        display: block;
+        background-color: #FF0C38;
+        margin-left: 10px;
+        margin: 0 10px;
+    }
+
+    h1{
+        font-size: 18px;
+        font-weight: 400;
+        display: flex;
+    }
+
+    button{
+        width: 60%;
+        padding: 16px 10px;
+        margin: 0 auto;
+        background-color: #FF0C38;
+        border-radius: 4px;
+        font-weight: bold;
+        color: var(--white-color);
+        font-size: 14px;
+
+        &:hover{
+            background-color: #a70926;
+        }
+    }
+`
