63fbf62ecaf6dc56fb01e2b56f8f94a51d1a2b5d
Natalia Francisca <nathifrancisca@gmail.com>
2023-11-28 22:50:38 -0300

    feat: created a function to generate only a h2 with the artists name

--- src/app/components/song/song.tsx
+++ src/app/components/song/song.tsx
@@ -4,16 +4,15 @@ import { SongComponent } from "./song.css";
 const Song = (obj: {song: ISong}) => {
     const {index, song_name, song_artist} = obj.song;
 
+    const generate_h2_names = () => song_artist.map((artist) => artist.name).join(", ");
+
     return(
         <SongComponent>
             <h1>{index}</h1>
 
             <div className="group-artist-info">
                 <h2 className="bold">{song_name}</h2>
-                <h2 className="artist-name">{song_artist.map((artist, key) => (
-                    <span key={key}>{artist.name}</span>
-                ))}
-                </h2>
+                <h2 className="artist-name">{generate_h2_names()}</h2>
             </div>
         </SongComponent>
     )
