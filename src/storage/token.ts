fae1dc597e5101bf123811fa171e11f5dc071ec6
Natalia Francisca <nathifrancisca@gmail.com>
2023-11-28 21:15:34 -0300

    feat: create a function to save the refresh token

--- src/storage/token.ts
+++ src/storage/token.ts
@@ -1,14 +1,21 @@
 'use server';
 import { cookies } from 'next/headers';
 
-export async function setToken(acessToken: string){
+export async function setToken(acessToken: string, refreshToken: string){
     const oneHour = 60 * 60 * 1000;
     cookies().set('USER_TOKEN', acessToken, { secure: true, expires: Date.now() + oneHour})
+    cookies().set('REFRESH_TOKEN', refreshToken, {secure: true})
 }
 
 export async function getToken(){
     const cookie = cookies();
     const token = cookie.get('USER_TOKEN');
+    return token?.value;
+}
+
+export async function getRefreshToken(){
+    const cookie = cookies();
+    const token = cookie.get('REFRESH_TOKEN');
     return token?.value;
 }
 
