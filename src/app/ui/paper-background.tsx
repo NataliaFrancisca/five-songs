4dff99537507cd188ae0fad4ed8b136dd61d0720
Natalia Francisca <nathifrancisca@gmail.com>
2023-12-20 21:00:46 -0300

    feat(style): add the style for the paper background

--- src/app/ui/paper-background.tsx
+++ src/app/ui/paper-background.tsx
@@ -1,8 +1,40 @@
 import styled from "styled-components";
 
 const PaperBackgroundComponent = styled.div`
+    width: 100%;
     display: grid;
     grid-template-columns: 60px auto;
+
+    #paper-lines{
+        display: grid;
+        position: absolute;
+        grid-template-rows: repeat(9, 70px);
+        width: 100%;
+
+        span{
+            height: 100%;
+            border-bottom: solid 2px rgba(26, 27, 28, 0.70);
+        }
+
+        span:last-child{
+            border-bottom: none;
+        }
+    }
+
+    #punched-paper-column{
+        display: grid;
+        grid-template-rows: repeat(9, 70px);
+        border-right: 2px solid rgba(194, 45, 45, 0.57);
+        align-items: center;
+        justify-content: center;
+
+        & span{
+            width: 24px;
+            height: 24px;
+            border-radius: 50%;
+            background-color: #1A1B1C;
+        }
+    }
 `
 
 const PaperBackground = () => {
